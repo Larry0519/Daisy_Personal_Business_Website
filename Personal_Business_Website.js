ac966deccf6af8ba6b9edbb16a7a41d8b6024df9
home <home@homes-MacBook-Pro.local>
2022-09-16 16:07:08 +1000

    footer section complete, to do carousel

--- Personal_Business_Website.js
+++ Personal_Business_Website.js
@@ -6,12 +6,33 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
+
+    
+
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 })
-        
+    
+
+    // typing animation script
+    let typed = new Typed(".typing",{
+        strings: ["COO at GW Technology", "Co-founder at GW Technology"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
    
